beb5ca491e61015b63620560d91c4f96087eda97
wesleymktd <wesleymktd@gmail.com>
2022-08-25 23:10:58 -0300

    consegui resolver a 9 uruuuu

--- src/challenges.js
+++ src/challenges.js
@@ -87,11 +87,44 @@ let strinArray = [];
 return strinArray;
 }
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(strings) {
+  let stringInvert = '';
+  for (let index = 0; index < strings.length; index += 1) {
+    if (strings[index] === 'a') {
+      stringInvert += 1;
+    } else if (strings[index] === 'e') {
+      stringInvert += 2;
+    } else if (strings[index] === 'i') {
+      stringInvert += 3;
+    } else if (strings[index] === 'o') {
+      stringInvert += 4;
+    } else if (strings[index] === 'u') {
+      stringInvert += 5;
+    } else {
+      stringInvert += strings[index];
+    }
+  }
+  return stringInvert;   
 }
-function decode() {
-  // seu código aqui
+function decode(revert) {
+  let stringRevert = '';
+
+  for (let index = 0; index < revert.length; index += 1) {
+    if (revert[index] === '1') {
+      stringRevert += 'a';
+    } else if (revert[index] === '2') {
+      stringRevert += 'e';
+    } else if (revert[index] === '3') {
+      stringRevert += 'i';
+    } else if (revert[index] === '4') {
+      stringRevert += 'o';
+    } else if (revert[index] === '5') {
+      stringRevert += 'u';
+    } else {
+      stringRevert += revert[index];
+    }
+  }
+  return stringRevert;
 }
 
 // Desafio 10
